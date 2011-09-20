57778d6856e64dec24214823fdb064425c0a98b5
Will McCoy <wmccoy@gmail.com>
2011-09-20 05:43:52 +0000

    Adding tagging support to missions admin stuff

--- heroesingreen/app/models/mission.rb
+++ heroesingreen/app/models/mission.rb
@@ -4,6 +4,23 @@ class Mission < ActiveRecord::Base
   has_many :mission_tag_mappings
   has_many :mission_tags, :through => :mission_tag_mappings 
 	
+ attr_accessor :mission_tag_ids
+ after_save :update_missions_tag
+ 
+ def update_missions_tag
+    unless mission_tag_ids.nil?
+      self.mission_tag_mappings.each do |m|
+        m.destroy unless mission_tag_ids.include?(m.mission_tag_id.to_s)
+        mission_tag_ids.delete(m.mission_tag_id.to_s)
+      end
+      mission_tag_ids.each do |g|
+        self.mission_tag_mappings.create(:mission_tag_id => g) unless g.blank?
+      end
+      reload
+      self.mission_tag_ids = nil
+    end
+ end
+	
 # => Type information
 	ONESHOT_TYPE = 0
 	REPEATABLE_TYPE = 1
--- heroesingreen/app/models/mission_tag.rb
+++ heroesingreen/app/models/mission_tag.rb
@@ -3,4 +3,21 @@ class MissionTag < ActiveRecord::Base
   has_many :missions, :through => :mission_tag_mappings 
   
   validates_presence_of :mission_tag
+  
+ attr_accessor :mission_ids
+ after_save :update_missions
+ 
+ def update_missions
+    unless mission_ids.nil?
+      self.mission_tag_mappings.each do |m|
+        m.destroy unless mission_ids.include?(m.mission_id.to_s)
+        mission_ids.delete(m.mission_id.to_s)
+      end
+      mission_ids.each do |g|
+        self.mission_tag_mappings.create(:mission_id => g) unless g.blank?
+      end
+      reload
+      self.mission_ids = nil
+    end
+ end
 end
