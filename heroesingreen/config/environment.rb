e400025079605f01d8e4374948f40a9d330ed2fd
Joshua Gerrish <jgerrish@gmail.com>
2012-02-01 04:17:29 +0000

    Use local gem repo for production on DreamHost

--- heroesingreen/config/environment.rb
+++ heroesingreen/config/environment.rb
@@ -3,6 +3,11 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
+# The following is required on DreamHost to use local gems
+if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
+  ENV['GEM_PATH'] = '/home/' + ENV['USER'] + '/.gems' + ':/usr/lib/ruby/gems/1.8'
+end
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
