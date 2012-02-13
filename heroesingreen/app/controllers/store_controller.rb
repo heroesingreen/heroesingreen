d793d4d212c0e2523d8515c464b1c24f6dbb1eb5
Vasumathi Lakshmanan <lvasumathi@gmail.com>
2012-02-13 01:46:16 +0000

    Add available number of points to the store view

--- heroesingreen/app/controllers/store_controller.rb
+++ heroesingreen/app/controllers/store_controller.rb
@@ -15,7 +15,7 @@ class StoreController < ApplicationController
     
     @garden = get_current_garden
     @garden.garden_tick
-    
+    @logged_in_user = get_user
     @plant_templates = PlantTemplate.all
       
       respond_to do |format|
