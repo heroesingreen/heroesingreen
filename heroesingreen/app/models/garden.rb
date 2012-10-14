f95e01e9e244cbb9b3d078659d9e8cd028688d1e
Joshua Gerrish <jgerrish@gmail.com>
2012-10-14 00:28:15 +0000

    Let a user plant anywhere.

--- heroesingreen/app/models/garden.rb
+++ heroesingreen/app/models/garden.rb
@@ -1,31 +1,42 @@
 class Garden < ActiveRecord::Base
   belongs_to :user
   has_many :grounds
-  
+
   validates_presence_of :user_id
   
   TIME_PER_TICK = 1.day
-  
+
+  @@DEFAULT_SIZE_X = 5
+  @@DEFAULT_SIZE_Y = 5
+
+  def self.DEFAULT_SIZE_X
+    @@DEFAULT_SIZE_X
+  end
+
+  def self.DEFAULT_SIZE_Y
+    @@DEFAULT_SIZE_Y
+  end
+
   def set_defaults
     self.default_wetness = 10
     self.default_nutrients = 7
     self.wetness_replenish = 2
     self.nutrients_replenish = 3
-    self.size_x = 5
-    self.size_y = 5 
+    self.size_x = @@DEFAULT_SIZE_X
+    self.size_y = @@DEFAULT_SIZE_Y
   end
-  
+
   def add_plant(plant_id, x, y)
-    if(is_compatible(plant_id, x, y))
+    #if(is_compatible(plant_id, x, y))
       found_ground = createGround(x, y)
       
       #need to check if there's a plant like this already here? TODO:agrandhi
       new_plant = PlantTemplate.find(plant_id).createPlant(found_ground)
       new_plant.save!
       return true
-    else
-      return false
-    end
+    #else
+    #  return false
+    #end
   end
  
   def is_there_a_good_ground(plant_type) 
@@ -83,20 +94,27 @@ class Garden < ActiveRecord::Base
   end
  
   def is_compatible(plant_template_id, x, y)  #can this plant go into this x and y
+    Rails.logger.info "got here 0"
     x = x.to_i
     y = y.to_i
     @plantTemplate = PlantTemplate.find(plant_template_id)
     if @plantTemplate == nil
       return false
     end
-    
+    Rails.logger.info "got here 0.1"
+
     if(self.grounds.find_by_x_and_y(x,y) != nil)
         return false
     end
+    Rails.logger.info "got here 0.2"
+    return true
+
     
     if(y < (6 - @plantTemplate.layer_max))
         return false
     end
+
+    Rails.logger.info "got here 1"
 
     inFrontOfY = y + 1
     while(inFrontOfY < self.size_y)  # check that there are no blocking plants in front of this x,y
@@ -110,6 +128,7 @@ class Garden < ActiveRecord::Base
     end
     inFrontOfY = inFrontOfY + 1
     end
+    Rails.logger.info "got here 2"
   
     if(@plantTemplate.shadow_block) # check that this is not blocking any plants behind this ground
     behindY = y - 1  
@@ -121,13 +140,14 @@ class Garden < ActiveRecord::Base
       behindY = behindY - 1
     end
     end
+    Rails.logger.info "got here 3"
 
     return true
   end
 
   
   def remove_plant(plant)
-    plant.destroy  #TODO Should we call plant.die here? Anything else to be done or just a destroy?
+    plant.ground.destroy  #TODO Should we call plant.die here? Anything else to be done or just a destroy?
   end
  
       
@@ -157,4 +177,21 @@ class Garden < ActiveRecord::Base
     self.updated_at=Time.now #TODO - use something other than updated_at
     self.save
   end
+
+  def get_plant_image_url(ground, x, y)
+    ground.plants.each do |plant|
+      if plant.plant_template.image_path != nil
+        img_url = "plants/#{plant.plant_template.image_path}"
+        if self.size_y - y == 1
+          img_url = img_url + "a.png"
+        elsif self.size_y - y == 2
+          img_url = img_url + "b.png"
+        elsif self.size_y - y == 3
+          img_url = img_url + "c.png"
+        end
+      end
+    end
+    return img_url
+  end
+
 end
