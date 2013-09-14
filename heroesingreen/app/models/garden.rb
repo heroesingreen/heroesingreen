f5c8e3933d05d52d455eb7362e7b7d2d1b1d62f3
Corinne Cooley <cooleycr@gmail.com>
2013-09-14 21:39:38 +0000

    Updating the garden to be wider/larger.

--- heroesingreen/app/models/garden.rb
+++ heroesingreen/app/models/garden.rb
@@ -6,7 +6,7 @@ class Garden < ActiveRecord::Base
   
   TIME_PER_TICK = 1.day
 
-  @@DEFAULT_SIZE_X = 5
+  @@DEFAULT_SIZE_X = 8
   @@DEFAULT_SIZE_Y = 5
 
   def self.DEFAULT_SIZE_X
@@ -41,7 +41,7 @@ class Garden < ActiveRecord::Base
  
   def is_there_a_good_ground(plant_type) 
     #which x and y
-    plantableGrounds = [[0,4],[0,5],[0,6],[1,4],[1,5],[1,6],[2,4],[2,5],[2,6],[3,4],[3,5],[3,6],[4,4],[4,5],[4,6],[5,4],[5,5],[5,6],[6,4],[6,5],[6,6],[7,4],[7,5],[7,6]]
+    plantableGrounds = [[0,0],[0,1],[0,2],[0,3],[0,4],[1,0],[1,1],[1,2],[1,3],[1,4],[2,0],[2,1],[2,2],[2,3],[2,4],[3,0],[3,1],[3,2],[3,3],[3,4],[4,0],[4,1],[4,2],[4,3],[4,4],[5,0],[5,1],[5,2],[5,3],[5,4],[5,5],[5,6],[6,0],[6,1],[6,2],[6,3],[6,4],[7,0],[7,1],[7,2],[7,3],[7,4]]
     
     #find vacant grounds
     vacantGrounds = []
